013c602d90ed8b4868b71a4d396dab4d990feac5
joel2035 <joel.pokam@hetic.net>
2019-01-08 04:00:10 +0100

    homeStyle

--- src/views/Category/Category.js
+++ src/views/Category/Category.js
@@ -1,26 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Category = ({ category, currentQuestionIndex, handleSubmit, answerInput }) => {
+const Category = ({
+  category,
+  currentQuestionIndex,
+  handleSubmit,
+  answerInput
+}) => {
   const currentQuestion = category.clues[currentQuestionIndex];
-  return (
-    <section>
-      <form onSubmit={handleSubmit}>
-        <h1>You choosed: {category.title}</h1>
-        <div className="question">
-          <h3 className="question__title">
-            {currentQuestion.question}
-          </h3>
-          <div className="question__answerInput">
-            {/* We give the ref below in order check the value */}
-            <input ref={answerInput} />
-          </div>
-          <button className="question__submit" type="submit">
-            Next
-          </button>
-        </div>
-      </form>
-    </section>
+  return ( <
+    section >
+    <
+    form onSubmit = {
+      handleSubmit
+    } >
+    <
+    div className = "contentTitle" > < h1 className = "title" > Category: {
+      category.title
+    } < /h1> </div >
+
+    <
+    div className = "question" >
+    <
+    h3 className = "question__title" > {
+      currentQuestion.question
+    } <
+    /h3> <
+    div className = "question__answerInput" > { /* We give the ref below in order check the value */ } <
+    input ref = {
+      answerInput
+    }
+    /> <
+    /div> <
+    button className = "question__submit"
+    type = "submit" >
+    Next <
+    /button> <
+    /div> <
+    /form> <
+    /section>
   );
 }
 
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,20 +1,47 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {
+  Link
+} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const Home = ({ categories }) => (
-  <section>
-    <h1>Homepage</h1>
-    {categories.length > 0 && (
-      <section>
-        {categories.map(category => (
-          <Link to={`/categories/${category.id}`} key={category.id}>
-            {category.title}
-          </Link>
-        ))}
-      </section>
-    )}
-  </section>
+const Home = ({
+  categories
+}) => ( < section className = "showcase" >
+  <
+  div className = "home" >
+  <
+  h1 className = "homeContent" > Choose a Category < /h1></div >
+
+  {
+    categories.length > 0 && ( < section className = "container" >
+      <
+      div className = "linkContent" >
+      <
+      ul className = "items" >
+
+      {
+        categories.map(category => ( < li className = "itemsLink" >
+          <
+          Link className = "link"
+          to = {
+            `/categories/${category.id}`
+          }
+          key = {
+            category.id
+          } > {
+            category.title
+          } < /Link> </li >
+        ))
+      }
+
+      <
+      /ul> </div >
+
+
+      <
+      /section>
+    )
+  } < /section>
 );
 
 Home.propTypes = {
